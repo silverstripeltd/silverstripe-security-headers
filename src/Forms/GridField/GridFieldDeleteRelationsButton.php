151a11a16d28272a3c71faedf7c1ff8d1b6aa6a2
Hitaishi Trivedi <hitaishi.trivedi@silverstripe.com>
2026-05-20 13:29:10 +1200

    Fixes

--- src/Forms/GridField/GridFieldDeleteRelationsButton.php
+++ src/Forms/GridField/GridFieldDeleteRelationsButton.php
@@ -10,7 +10,7 @@ use SilverStripe\Forms\GridField\GridField_FormAction;
 use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Core\Extensible;
 use SilverStripe\Security\Security;
-use SilverStripe\View\ArrayData;
+use SilverStripe\Model\ArrayData;
 use SilverStripe\Forms\FormAction;
 use SilverStripe\View\SSViewer;
 use SilverStripe\Forms\FieldList;
@@ -23,7 +23,7 @@ use SilverStripe\Forms\FormField;
 use SilverStripe\Forms\CheckboxField;
 use SilverStripe\Core\Manifest\ModuleLoader;
 use SilverStripe\Forms\ReadonlyField;
-use SilverStripe\ORM\ArrayList;
+use SilverStripe\Model\List\ArrayList;
 use SilverStripe\View\Requirements;
 use UncleCheese\DisplayLogic\Forms\Wrapper;
 
